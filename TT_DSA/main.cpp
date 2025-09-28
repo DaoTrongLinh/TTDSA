c4e0bab5d41e1773c1d7db484d7e3e737d5e3b0d
Đào Trọng Linh <24022678@vnu.edu.vn>
2025-09-28 20:03:59 +0700

    Them comment

--- TT_DSA/main.cpp
+++ TT_DSA/main.cpp
@@ -18,9 +18,9 @@ int Balo(int W, vector<int>& w, vector<int>& v, int n) {
     for(int i = 1; i <= n; i++) {
         for(int j = 1; j <= W; j++) {
             if(w[i-1] <= j) { //co the chon do vat i - 1
-                dp[i][j] = max(dp[i-1][j], v[i-1] + dp[i-1][j - w[i-1]]);
+                dp[i][j] = max(dp[i-1][j], v[i-1] + dp[i-1][j - w[i-1]]); //neu chon vat i
             } else {
-                dp[i][j] = dp[i-1][j];
+                dp[i][j] = dp[i-1][j]; //neu khong chon vat i
             }
         }
     }
