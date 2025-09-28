46002d6549e8df15116d576868b8e393425cb1c7
Đào Trọng Linh <24022678@vnu.edu.vn>
2025-09-28 21:52:11 +0700

    them tiep comment

--- TT_DSA/main.cpp
+++ TT_DSA/main.cpp
@@ -15,6 +15,9 @@ int Fibonacci(int N) {
 int Balo(int W, vector<int>& w, vector<int>& v, int n) {
     vector<vector<int>> dp(n+1, vector<int>(W+1, 0));
 
+    //i la do vat thu i
+    //j la gioi han trong luong hien tai
+    //dp[i][j] la tong gia tri lon nhat cua cac do vat lay duoc khi chon trong cac do vat tu 1 toi i va j la gioi han tong trong luong cua chung
     for(int i = 1; i <= n; i++) {
         for(int j = 1; j <= W; j++) {
             if(w[i-1] <= j) { //co the chon do vat i - 1
@@ -33,8 +36,8 @@ int main() {
 
     vector<int> w = {2, 3, 4, 5};
     vector<int> v = {3, 4, 5, 6};
-    int W = 5;
     int n = w.size();
+    int W = 5;
     cout << "Max value = " << Balo(W, w, v, n) << endl;
 
     return 0;
